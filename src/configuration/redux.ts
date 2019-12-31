802a027ab550d829eceddd760a1c05df713e6a65
Vincent Ting <homerdd@gmail.com>
2020-01-01 00:01:52 +0800

    refactor: 💡 improve the type support for redux

--- src/configuration/redux.ts
+++ src/configuration/redux.ts
@@ -1,21 +1,21 @@
+/* eslint-disable no-unused-vars */
 import { applyMiddleware, createStore, compose, combineReducers } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import logger from 'redux-logger'
 import { all, fork } from 'redux-saga/effects'
 
+import wrdAutocompleteReducer, * as WordAutocompleteDuck from '../ducks/word-autocomplete'
+
 export const composeEnhancers = (process.env.NODE_ENV !== 'production' &&
   (window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)) || compose
 
-const duckContext = (require as any).context('../ducks', true, /\/index.ts$/)
-
-const ducks: any[] = duckContext.keys().map((duckFile: string) => {
-  const { default: reducer, watchSaga, namespace } = duckContext(duckFile)
-  return {
-    reducer, watchSaga, namespace
-  }
-})
+const ducks = [WordAutocompleteDuck]
 
 const { STATE_FROM_SERVER } = (window as any)
+
+export interface StateInterface {
+  [WordAutocompleteDuck.namespace]: typeof WordAutocompleteDuck.initialState
+}
 
 export default function configureStore () {
   const sagaMiddleware = createSagaMiddleware()
@@ -26,10 +26,9 @@ export default function configureStore () {
     yield all(ducks.map(({ watchSaga }) => fork(watchSaga)))
   }
 
-  const store = createStore(combineReducers(ducks.reduce((pre, { reducer, namespace }) => ({
-    ...pre,
-    [namespace]: reducer
-  }), {})), STATE_FROM_SERVER || {}, composedEnhancers)
+  const store = createStore(combineReducers({
+    [WordAutocompleteDuck.namespace]: wrdAutocompleteReducer
+  }), STATE_FROM_SERVER || {}, composedEnhancers)
 
   sagaMiddleware.run(rootSaga)
   return store
