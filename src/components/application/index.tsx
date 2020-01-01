aa3834059b97f8759e85089213a561d29df9cf52
Vincent Ting <homerdd@gmail.com>
2020-01-01 14:42:01 +0800

    feat: 🎸 feat: add grommet as the UI component lib

--- src/components/application/index.tsx
+++ src/components/application/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
+import { Box } from 'grommet'
 
 export default function NoteBookApplication () {
-  return (<span>Hello world</span>)
+  return (<Box>Hello world</Box>)
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { Grommet } from 'grommet'
 
 import NoteBookApplication from './components/application'
 import './configuration/axios'
 import configureStore from './configuration/redux'
+import theme from './configuration/theme'
 
 const store = configureStore()
 
 ReactDOM.render((
   <Provider store={store}>
-    < NoteBookApplication / >
+    <Grommet theme={theme}>
+      < NoteBookApplication / >
+    </Grommet>
   </Provider>
 ), document.getElementById('root'))
